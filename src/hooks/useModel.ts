2042c79412add311b5368da5ade10f2fa7850efd
isaachan <isaachanstar@gmail.com>
2026-04-22 15:23:17 +0800

    clean up

--- src/hooks/useModel.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { ModelContext } from '../store/ModelContext';
-
-export const useModel = () => {
-  return useContext(ModelContext);
-};
--- src/utils/id.ts
+++
@@ -1,6 +0,0 @@
-let counter = 0;
-
-export const generateId = (prefix: string): string => {
-  counter += 1;
-  return `${prefix}-${Date.now()}-${counter}`;
-};
